9c80fb6495b7d816aa4340433ec184a07fe3b06a
Mike Norton <manorton@gmail.com>
2016-03-24 17:54:38 -0500

    Git Fix

---
+++ Assets/Corcra Studio/Item System/Scripts/Editor/ISObject Editor/ISObjectEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+namespace CorcraStudio.ItemSystem.Editor
+{
+    public partial class ISObjectEditor : EditorWindow
+    {
+        [MenuItem("Corcra Studio/Database/Item System Editor %#i")]
+        public static void Init()
+        {
+            ISObjectEditor window = EditorWindow.GetWindow<ISObjectEditor>();
+            window.minSize = new Vector2(800, 600);
+            window.titleContent.text = ("Item System");
+            window.Show();
+        }
+
+
+        void OnEnable()
+        {
+
+        }
+
+
+        void OnGUI()
+        {
+            TopTabBar();
+
+            GUILayout.BeginHorizontal();
+            ListView();
+            ItemDetails();
+            GUILayout.EndHorizontal();
+
+            BottomStatusBar();
+        }
+    }
+}
---
+++ Assets/Corcra Studio/Item System/Scripts/Editor/ISObject Editor/ISObjectTopTabBar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CorcraStudio.ItemSystem.Editor
+{
+    public partial class ISObjectEditor
+    {
+ 
+        void TopTabBar()
+        {
+            GUILayout.BeginHorizontal("Box", GUILayout.ExpandWidth(true));
+
+            WeaponTab();
+            ArmorTab();
+            GUILayout.Button("Potions");
+            AboutTab();
+
+            GUILayout.EndHorizontal();
+
+        }
+
+        void WeaponTab()
+        {
+            GUILayout.Button("Weapons");
+        }
+
+
+        void ArmorTab()
+        {
+            GUILayout.Button("Armor");
+        }
+
+
+        void AboutTab()
+        {
+            GUILayout.Button("About");
+        }
+    }
+}
