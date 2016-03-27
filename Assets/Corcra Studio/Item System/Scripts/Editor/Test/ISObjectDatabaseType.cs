2d123c98d5fc0a7ecd8965ed6d19c3de002eca51
Mike Norton <manorton@gmail.com>
2016-03-27 11:29:20 -0500

    Missed Changed

---
+++ Assets/Corcra Studio/Item System/Scripts/Editor/Test/ISObjectDatabaseType.cs
@@ -0,0 +1,66 @@
+﻿/// <summary>
+/// ISObjectDatabaseType.cs
+/// March 27, 2016
+/// Mike Norton - Corcra Studio
+/// 
+/// </summary>
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace CorcraStudio.ItemSystem.Editor
+{
+    public class ISObjectDatabaseType<D, T> where D : ScriptableObjectDatabase<T> where T : ISObject
+    {
+        [SerializeField]
+        D database;
+        [SerializeField]
+        string dbName;
+        [SerializeField]
+        string dbPath = @"Database";
+
+
+
+        public ISObjectDatabaseType(string n)
+        {
+            dbName = n;
+        }
+
+
+
+        public void OnEnable()
+        {
+            if (database == null)
+                LoadDatabase();
+        }
+
+
+
+        void LoadDatabase()
+        {
+            string dbFullPath = @"Assets/" + dbPath + "/" + dbName;
+
+            //load the database
+            database = AssetDatabase.LoadAssetAtPath(dbFullPath, typeof(D)) as D;
+
+
+            //if we could not load the database, create one
+            if (database == null)
+                CreateDatabase(dbFullPath);
+        }
+
+
+        void CreateDatabase(string dbFullPath)
+        {
+            //check to see if the folder exists, if not create it
+            if (!AssetDatabase.IsValidFolder("Assets/" + dbPath))
+                AssetDatabase.CreateFolder("Assets", dbPath);
+
+            //create the database and refresh the AssetsDatabase
+            database = ScriptableObject.CreateInstance<D>() as D;
+            AssetDatabase.CreateAsset(database, dbFullPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
